4e6eaaa8f897fc08e34c7e490d4349e08b4b2b60
facronactz <facronactz@gmail.com>
2025-02-02 21:20:08 +0700

    feat(auth): add logout functionality

---
+++ src/views/LogoutView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { useAuthStore } from '@/stores/auth.js'
+
+const authStore = useAuthStore()
+authStore.logout()
+
+setTimeout(() => {
+  window.location.href = '/'
+}, 2000)
+</script>
+
+<template>
+Please wait while you are logged out...
+</template>
+
+<style scoped>
+
+</style>
